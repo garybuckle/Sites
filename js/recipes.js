a653c8c6f7c814831a4f02251dad25b72142a335
Gary Buckle <garybuckle@Garys-MacBook-Pro-16.local>
2020-04-15 17:52:54 +0100

    js hide toggle working

--- js/recipes.js
+++ js/recipes.js
@@ -1,7 +1,30 @@
 var linkToHide = document.getElementsByClassName("to-hide");
+// console.log(" Stuff to hide");
+// console.log("blah blah");
 console.log(linkToHide);
 linkToHide[0].style.display = "none";
-linkToHide[1].style.display = "none";
 
+
+
+// linkToHide[1].style.display = "none";
+
+var clickToCollapse = document.getElementsByClassName("clickMe");
+console.log("The link to click on");
+console.log(clickToCollapse);
+
+clickToCollapse[0].addEventListener("click", toggleText);
+
+function toggleText(e) {
+    e.preventDefault();
+    console.log("clicked");
+if (linkToHide[0].style.display === "none") {
+    linkToHide[0].style.display = "block";
+} else {
+    linkToHide[0].style.display = "none";
+}
+    
+
+
+}
 
 
