96d8a5a26f6d5cd92c58d2ff2dd7493cf8e2ac15
Gary Buckle <buckle.g@mac.com>
2020-06-10 17:20:29 +0100

    All code sessions

--- js/recipes.js
+++ js/recipes.js
@@ -69,6 +69,28 @@ $(document).ready(function(){
 
   });
 
+
+  $('#recipe-carousel').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
     
 
 
