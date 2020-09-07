c0aaac313591d569309d7cbdc7059331734fb643
Gary Buckle <buckle.g@mac.com>
2020-09-07 09:20:27 +0100

    jquery d-none on cells

---
+++ game/scripts/scripts.js
@@ -0,0 +1,10 @@
+$(document ).ready(function() {
+    console.log( "Document is ready!" );
+    //grab an element as a test
+
+    $('.cell2').addClass("d-none");
+    $('.cell7').addClass("d-none");
+    });
+
+
+
