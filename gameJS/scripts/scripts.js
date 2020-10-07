16372cdd93c76da617cefd0bec677d95c0712c21
Gary Buckle <buckle.g@mac.com>
2020-10-07 10:07:28 +0100

    Step one game part 4

--- gameJS/scripts/scripts.js
+++ gameJS/scripts/scripts.js
@@ -1,14 +1,18 @@
-// JavaScipt equivalent of gamme not using JQuery
+// JavaScipt equivalent of game not using JQuery
 
-// get the images and dump to the console so we can see what is grabbed
+// get the images and put them into an array 
+// so we can see what is grabbed, dump to the console 
+// 
 var allImages = document.images;
 console.log(allImages);
 // We can manipulate the images a bit to test
+// print the contents of the secon image
+// to the console along with amount of images
 console.log("Second Image " + allImages[1].src);
 console.log ("There are " + allImages.length + " images");
 
 
-// hide an image to test, grab the second image or the 7th image
-// Remember that arrays strat at zero.
+// hide an image to test, grab the second image 
+// or the 7th image.Remember that arrays start at zero.
 
 allImages[6].style.display = "none";
