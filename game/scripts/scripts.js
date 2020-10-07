a516afa35e63bc64316df57be1c07771549512f3
Gary Buckle <buckle.g@mac.com>
2020-10-07 09:03:35 +0100

    Changed JS

--- game/scripts/scripts.js
+++ game/scripts/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
     console.log("ready");
     // $('#img1').hide();
@@ -14,15 +13,3 @@ $(document).ready(function(){
     $(images).eq(7).addClass('d-block');
     // console.log($(images).eq(3));
 });
-=======
-$(document ).ready(function() {
-    console.log( "Document is ready!" );
-    //grab an element as a test
-
-    $('.cell2').addClass("d-none");
-    $('.cell7').addClass("d-none");
-    });
-
-
-
->>>>>>> c0aaac313591d569309d7cbdc7059331734fb643
