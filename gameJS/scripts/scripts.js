4dccb3de81304295dcc7f23795b72f263f53a2c0
Gary Buckle <buckle.g@mac.com>
2020-10-08 11:48:45 +0100

    set up functions for hide and random

--- gameJS/scripts/scripts.js
+++ gameJS/scripts/scripts.js
@@ -4,15 +4,33 @@
 // so we can see what is grabbed, dump to the console 
 // 
 var allImages = document.images;
-console.log(allImages);
+// console.log(allImages);
 // We can manipulate the images a bit to test
 // print the contents of the secon image
 // to the console along with amount of images
-console.log("Second Image " + allImages[1].src);
-console.log ("There are " + allImages.length + " images");
+// console.log("Second Image " + allImages[1].src);
+// console.log ("There are " + allImages.length + " images");
 
 
 // hide an image to test, grab the second image 
 // or the 7th image.Remember that arrays start at zero.
 
-allImages[6].style.display = "none";
+// allImages[6].style.display = "none";
+
+hideAll;
+
+function selectImage() {
+    
+}
+
+function hideAll() {
+    for (var i = 0; allImages.length-1; i++) {
+        allImages[i].style.display = "none";
+        
+    }
+    
+}
+
+function checkSelection() {
+    
+}
