29034eeafcf9cbd32bcdc67b84606fbedfcb69a9
Gary Buckle <buckle.g@mac.com>
2020-10-09 09:35:37 +0100

    Typos

--- gameJS/scripts/scripts.js
+++ gameJS/scripts/scripts.js
@@ -6,7 +6,7 @@
 var allImages = document.images;
 console.log(allImages);
 // We can manipulate the images a bit to test
-// print the contents of the secon image
+// print the contents of the second image
 // to the console along with amount of images
 console.log("Second Image " + allImages[1].src);
 console.log ("There are " + allImages.length + " images");
